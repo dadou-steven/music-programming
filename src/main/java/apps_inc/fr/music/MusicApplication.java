4eba0c91c8b5af7353c0173d5012c024f34d6f19
DADOU STEVEN <dgv1pro@gmail.com>
2023-09-03 15:25:02 +0200

    first commit

---
+++ src/main/java/apps_inc/fr/music/MusicApplication.java
@@ -0,0 +1,13 @@
+package apps_inc.fr.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apps_inc/fr/music/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package apps_inc.fr.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
